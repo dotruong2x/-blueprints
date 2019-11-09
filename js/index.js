84c57efa14e9d8e822ecfcf269fb19fc00efea43
quyennt <quyennt@dft.vn>
2019-11-09 14:04:09 +0700

    a

--- js/index.js
+++ js/index.js
@@ -60,17 +60,47 @@ $('#slide-4').owlCarousel({
     margin:30,
     nav:false,
     dots:false,
-    autoplay:false,
+    autoplay:true,
     autoplayTimeout:1000,
     responsive:{
         0:{
-            items:5
+            items:6
         },
         600:{
-            items:5
+            items:6
         },
         1000:{
-            items:5
+            items:6
         }
     }
 });
+
+// chi tiết sản phẩm
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
